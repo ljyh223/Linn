ac97ecca1acdf0f3a1c85f4688016c7a9d637b1f
ljyh223 <3439426154@qq.com>
2026-04-29 19:22:14 +0800

    publish tag v1.0

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use crate::{api::init_client, db::Db, ui::window::Window};
 use relm4::{
     RelmApp,
-    gtk::gio::{self, Settings, prelude::SettingsExt},
+    gtk::gio::{Settings, prelude::SettingsExt},
 };
-use std::{env, process::Command, sync::{Arc, Mutex}};
+use std::sync::{Arc, Mutex};
 
 mod api;
 mod db;
@@ -17,32 +17,36 @@ pub const APP_NAME: &str = "Linn";
 mod icon_names {
     include!(concat!(env!("OUT_DIR"), "/icon_names.rs"));
 }
+
+const STYLE_CSS: &str = include_str!("style.css");
+
 fn get_cookie() -> String {
     let settings = Settings::new(APPLICATION_ID);
     settings.string("cookie").to_string()
 }
 
 fn main() {
-    Command::new("glib-compile-schemas")
-        .arg("data")
-        .status()
-        .expect("compile schemas failed");
-
-    unsafe {
-        #[cfg(debug_assertions)]
-        env::set_var("GSETTINGS_SCHEMA_DIR", "data");
+    #[cfg(debug_assertions)]
+    {
+        std::process::Command::new("glib-compile-schemas")
+            .arg("data")
+            .status()
+            .expect("compile schemas failed");
+        unsafe {
+            std::env::set_var("GSETTINGS_SCHEMA_DIR", "data");
+        }
     }
+
     eprintln!("Starting Linn...");
 
     let cookie = get_cookie();
     init_client(cookie.clone());
-    let css_path = std::env::current_dir().unwrap().join("src/style.css");
 
     let db = Arc::new(Mutex::new(Db::open().expect("Failed to open database")));
 
     relm4_icons::initialize_icons(icon_names::GRESOURCE_BYTES, icon_names::RESOURCE_PREFIX);
     gst::init().expect("Failed to initialize GStreamer");
-    relm4::set_global_css_from_file(&css_path).expect("load CSS failed");
+    relm4::set_global_css(STYLE_CSS);
 
     let app = RelmApp::new(APPLICATION_ID);
     app.run::<Window>((cookie, db));
